51a83ed8f8938b13a5345818192b508f959d153e
csteger <christian.steger@env.ethz.ch>
2022-03-01 16:50:37 +0100

    Layout of file 'setup_python.py' modified

--- setup_python.py
+++ setup_python.py
@@ -9,24 +9,15 @@
 import importlib
 import py_compile
 
-###############################################################################
 # Check availability of external packages
-###############################################################################
-
-# List of packages
 packages = ("numpy", "scipy", "geographiclib", "pyproj", "osgeo",
             "shapely", "fiona", "pygeos", "skimage")
-
-# Check if available
 for i in packages:
     if importlib.util.find_spec(i) is None:
         raise ImportError("Package " + i + " not installed")
 print("All required Python packages are available.")
 
-###############################################################################
 # Compile functions
-###############################################################################
-
 py_compile.compile("src/auxiliary.py", cfile="lib/auxiliary.pyc")
 py_compile.compile("src/geoid.py", cfile="lib/geoid.pyc")
 py_compile.compile("src/load_dem.py", cfile="lib/load_dem.pyc")
