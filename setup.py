4106cd0fcf62ea8c02d423993c3de6545791f9de
Christian Steger <csteger@Christians-MBP.lan>
2022-08-06 14:57:19 +0200

    Setup file modified

--- setup.py
+++ setup.py
@@ -17,9 +17,6 @@ import numpy as np
 # Manual settings
 # -----------------------------------------------------------------------------
 
-# Path to manually installed NetCDF4 C++ library (Mac OS X; compiled with gcc)
-path_lib_netcdf = "/opt/local/lib/"
-
 # NetCDF 3/4 library
 lib_netcdf = ["libnetcdf", "libnetcdf_c++4"]  # NetCDF4
 # lib_netcdf = ["libnetcdf_c++"]  # NetCDF3
@@ -32,49 +29,48 @@ path_lib_conda = os.environ["CONDA_PREFIX"] + "/lib/"
 if sys.platform in ["linux", "linux2"]:
     print("Operating system: Linux")
     lib_end = ".so"
-    libraries_cython = ["m", "pthread"]
-    include_dirs_cpp = [np.get_include()]
-    extra_objects_cpp = [path_lib_conda + "libembree3" + lib_end] \
-        + [path_lib_conda + i + lib_end for i in lib_netcdf]
+    compiler = "gcc"
+    extra_compile_args_cython = ["-O3", "-ffast-math", "-fopenmp"]
 elif sys.platform in ["darwin"]:
     print("Operating system: Mac OS X")
     lib_end = ".dylib"
-    libraries_cython = ["m", "iomp5", "pthread"]
-    include_dirs_cpp = [np.get_include()]
-    extra_objects_cpp = [path_lib_conda + "libembree3" + lib_end] \
-        + [path_lib_netcdf + i + lib_end for i in lib_netcdf]
-    for i in extra_objects_cpp:
-        if not os.path.isfile(i):
-            raise ValueError("Library " + i + " not found")
+    compiler = "clang"
+    extra_compile_args_cython = ["-O3", "-ffast-math",
+                                 "-Wl,-rpath," + path_lib_conda,
+                                 "-L" + path_lib_conda, "-fopenmp"]
 elif sys.platform in ["win32"]:
     print("Operating system: Windows")
     print("Warning: Package not yet tested for Windows")
 else:
     raise ValueError("Unsupported operating system")
+libraries_cython = ["m", "pthread"]
+include_dirs_cpp = [np.get_include()]
+extra_objects_cpp = [path_lib_conda + i + lib_end for i in ["libembree3"]
+                     + lib_netcdf]
 
 # -----------------------------------------------------------------------------
 # Compile Cython/C++ code
 # -----------------------------------------------------------------------------
 
-os.environ["CC"] = "gcc"
+os.environ["CC"] = compiler
 
 ext_modules = [
     Extension("horayzon.transform",
               ["horayzon/transform.pyx"],
               libraries=libraries_cython,
-              extra_compile_args=["-O3", "-ffast-math", "-fopenmp"],
+              extra_compile_args=extra_compile_args_cython,
               extra_link_args=["-fopenmp"],
               include_dirs=[np.get_include()]),
     Extension("horayzon.direction",
               ["horayzon/direction.pyx"],
               libraries=libraries_cython,
-              extra_compile_args=["-O3", "-ffast-math", "-fopenmp"],
+              extra_compile_args=extra_compile_args_cython,
               extra_link_args=["-fopenmp"],
               include_dirs=[np.get_include()]),
     Extension("horayzon.topo_param",
               ["horayzon/topo_param.pyx"],
               libraries=libraries_cython,
-              extra_compile_args=["-O3", "-ffast-math", "-fopenmp"],
+              extra_compile_args=extra_compile_args_cython,
               extra_link_args=["-fopenmp"],
               include_dirs=[np.get_include()]),
     Extension("horayzon.horizon",
